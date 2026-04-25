9503e0d844e51f98fd4c4721abcc0ea3f13f69f4
Aniket <karanjekaraniket.com>
2026-04-25 14:35:20 +0530

    changes

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import factory.DriverFactory;
@@ -19,7 +20,7 @@ public class BaseTest {
 
     @BeforeClass
     @Parameters({ "browser" })
-    public void setUp(String browser) throws IOException {
+    public void setUp(@Optional("chrome") String browser) throws IOException {
         logger = LogManager.getLogger(this.getClass());
         driver = DriverFactory.initDriver(browser);
     }
